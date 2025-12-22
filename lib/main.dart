459f8de457cc577ba7aca49bbab9571e98240c7f
saif bayyari <saifbayyari@protonmail.com>
2025-12-21 19:43:34 -0500

    added some fonts, app icons, and asset images

--- lib/main.dart
+++ lib/main.dart
@@ -5,43 +5,52 @@ import 'package:flutter/material.dart';
 
 //The main function is the starting point for all of our flutter apps.
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(Felinder());
+}
+
+class Felinder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.brown[50],
         appBar: AppBar(
-          title: const Text('Felinder: A Dating Platform For Cats'),
+          title: const Text('Felinder! A Dating Platform For Cats', style: TextStyle(fontSize: 20)),
           backgroundColor: Color.fromRGBO(255, 105, 180, 1),
           actions: [
             IconButton(
               icon: const Icon(Icons.add_alert),
               tooltip: 'Show Snackbar',
               onPressed: () {
-                print("hello");
+                print("hello saif");
               },
             ),
           ],
         ),
         body: Column(
-          children:[AutoSizeText(
-            'Felinder',
+          children: [
+            AutoSizeText(
+              'Felinder',
 
-            style: TextStyle(
-              fontFamily: 'Pacifico',
-              color: Color.fromRGBO(255, 105, 180, 1),
-              fontSize: 48,
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Color.fromRGBO(255, 105, 180, 1),
+                fontSize: 48,
+              ),
+
+              maxLines: 1,
+              minFontSize: 20,
             ),
-
-            maxLines: 1,
-            minFontSize: 20,
-          ),
             Image(
-              image: NetworkImage("https://paradepets.com/.image/c_fill,w_1200,h_1200,g_faces:center/NTowMDAwMDAwMDAwMDI4ODAw/shutterstock_2502425875.jpg"),
+              //image: AssetImage('images/happy-tabby-cat.png'),
+              alignment: Alignment.centerRight,
+              image: NetworkImage(
+                "https://paradepets.com/.image/c_fill,w_1200,h_1200,g_faces:center/NTowMDAwMDAwMDAwMDI4ODAw/shutterstock_2502425875.jpg",
+              ),
             ),
-        ],
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
