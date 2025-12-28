ab9b591015b9eb94a1399adf6e0c776300572511
saif bayyari <saifbayyari@protonmail.com>
2025-12-28 09:39:50 -0500

    Successfully completed Flutter Layouts Challenge (The Complete Flutter Development Bootcamp with Dart - By Dr Angela Yu from Udemy)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 //Felinder: Dating App For Cats
@@ -14,42 +13,31 @@ class Felinder extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Felinder! A Dating Platform For Cats',
-            style: TextStyle(fontSize: 20),
-          ),
-          backgroundColor: Color.fromRGBO(255, 105, 180, 1),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.add_alert),
-              tooltip: 'Show Snackbar',
-              onPressed: () {
-                print("hello saif");
-              },
-            ),
-          ],
-        ),
-        body: SafeArea(
-          child: Container(
-            margin: EdgeInsets.all(25.0),
-            //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0)
-            child: Center(
-              child: AutoSizeText(
-                'Felinder',
 
-                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                  fontFamily: 'Pacifico',
-                  color: const Color.fromRGBO(255, 105, 180, 1),
-                ),
-                maxLines: 1,
-                minFontSize: 70,
-                maxFontSize: 120,
+        backgroundColor: Colors.teal,
+
+
+        body: SafeArea(
+          child: Row(
+
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0)
+            children: [
+              Container(width: 100, color: Colors.red),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(width: 100, height: 100, color: Colors.yellow),
+                  Container(width: 100, height: 100, color: Colors.green)
+                ],
               ),
+              Container(width: 100, color: Colors.blue)
+
+
+            ],
             ),
           ),
         ),
-      ),
     );
   }
 }
