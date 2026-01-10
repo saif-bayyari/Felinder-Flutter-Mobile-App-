c54b916e439bac4ae90aecd7f70bd99b7cc461f9
saif bayyari <saifbayyari@protonmail.com>
2026-01-10 13:30:23 -0500

    new dev branch, init commit - added sign in buttons with custom icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:auto_size_text/auto_size_text.dart';
+
 import 'package:flutter/material.dart';
 
 //Felinder: Dating App For Cats
@@ -16,7 +16,7 @@ class Felinder extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
-            'Felinder! A Dating Platform For Cats',
+            '',
             style: TextStyle(fontSize: 20),
           ),
           backgroundColor: Color.fromRGBO(255, 105, 180, 1),
@@ -33,35 +33,69 @@ class Felinder extends StatelessWidget {
         body: SafeArea(
           child: Column(
 
-              //main axis alignment basically is saying "where does the column start attaching things relative to?"
-              //mainAxisAlignment: MainAxisAlignment.center,
-              //crossAxisAlignment: CrossAxisAlignment.center,
-              verticalDirection: VerticalDirection.up,
-              //mainAxisSize: MainAxisSize.min,
-              children: [
+            //main axis alignment basically is saying "where does the column start attaching things relative to?"
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            //mainAxisSize: MainAxisSize.min,
+            children: [
 
-                Container(width: 100, height: 100, color: Colors.red),
-                Container(width: 100, height: 100, color: Colors.green),
-                Container(width: 100, height: 100, color: Colors.yellow),
-                Center(
-                child: AutoSizeText(
-                  'Felinder',
 
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineLarge
-                      ?.copyWith(
-                    fontFamily: 'Pacifico',
-                    color: const Color.fromRGBO(255, 105, 180, 1),
-                  ),
-                  maxLines: 1,
-                  minFontSize: 70,
-                  maxFontSize: 120,
+              Center(
+                child: Text(
+                    'Felinder',
+
+                    style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.pinkAccent,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                        fontFamily: "Pacifico"
+                    )
                 ),
               ),
-              Container(width: 100, height: 100, color: Colors.red),
-                Container(width: 100, height: 100, color: Colors.blue)
-              ]
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Column(
+                  children: [
+                    ListTile(
+                      titleAlignment: ListTileTitleAlignment.center,
+                      leading:Icon(Icons.phone, color: Colors.teal),
+                      title:
+                      TextButton(
+                        child: const Text('SIGN IN WITH PHONE NUMBER'),
+                        onPressed: () {
+                          // Action for the first button
+                          print("test");
+                        },
+                      ),
+                    ),
+                    ListTile(
+                      titleAlignment: ListTileTitleAlignment.center,
+                      leading:
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Container(
+                          height: 25,
+                          child: Image.asset("lib/icons/google-color-icon.png"),
+                        ),
+                      )
+                      
+                      ,
+                      title:
+                      TextButton(
+                        child: const Text('SIGN IN WITH GOOGLE'),
+                        onPressed: () {
+                          // Action for the first button
+                          print("test");
+                        },
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
